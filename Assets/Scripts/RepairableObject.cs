d4ab7c6e7b6e900ef3491359f666ef17c50678a8
Joel Lee <lee.joel395@gmail.com>
2018-07-17 04:53:14 -0700

    Added in quicktime event style minigame to repairing rooms. Fixed orientation of the test level.
    QTE-like minigame uses a stack to build a solution, then checks input to the top of the stack and decides whether to pop or not.

--- Assets/Scripts/RepairableObject.cs
+++ Assets/Scripts/RepairableObject.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using JulyJam.Player;
 using JulyJam.UI;
 using MovementEffects;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace JulyJam.Interactables{
     /// <summary>
@@ -16,6 +21,7 @@ namespace JulyJam.Interactables{
         [SerializeField] private float _partHealthDrain = 1f; //how much health does this part drain when damaged
         [SerializeField] private float _irrepairableTime = 90f; //time it takes to permanently damage the ship
         [SerializeField] private float _totalShipDamage = 0f; //how much damage to the ship does this do when destroyed?
+        [SerializeField] private float _shipRecoverValue = 20f; //how much does the ship heal by when repaired?
         [SerializeField] private bool _isBroken;
         private bool _isDestroyed;
 
@@ -28,21 +34,32 @@ namespace JulyJam.Interactables{
 
         //a part is broken, add drain to ship
         public delegate void PartBrokenEvent(float drain);
-
         public PartBrokenEvent PartBroken;
 
         //a part is repaired, remove drain from ship
         public delegate void PartRepairedEvent(float drain);
-
         public PartRepairedEvent PartRepaired;
+
+        //a part was partially repaired, heal the ship a little bit
+        public delegate void ShipHealedEvent(float value);
+        public ShipHealedEvent HealShip;
 
         public GameObject needsRepairMarker;
         public GameObject isDestroyedMarker;
+        public Text inputDirectorField;
+
         private float _timeSinceLastCheck = 0f;
         private float _destroyTimer = 0f; //time until the part is destroyed
 
+        [Range(1, 4)] public int difficulty;
+        private Stack<char> _solutionStack; //using a stack to store our solution, because input order matters
+        private readonly char[] _validKeys = new[]{'H', 'J', 'K', 'L'}; //all valid key presses
+
+        private PlayerMovement _player;
+
         void Start(){
             needsRepairMarker.SetActive(false);
+            _solutionStack = new Stack<char>(Mathf.CeilToInt(difficulty)); //set the max cap of our stack to the difficulty
         }
 
         // Update is called once per frame
@@ -77,11 +94,37 @@ namespace JulyJam.Interactables{
         public void BreakObject(){
             _isBroken = true;
             _destroyTimer = 0f;
+
+            CreateSolutionStack(); //generate a new solution when broken
+
             PartBroken(_partHealthDrain);
             needsRepairMarker.SetActive(true);
             irrepairableTimerUI.gameObject.SetActive(true);
             //start counting draining health until
             //do some fx and sprite swaps in here
+        }
+
+        /// <summary>
+        /// create a randomized stack of keys we need to press for this room
+        /// </summary>
+        private void CreateSolutionStack(){
+            StringBuilder sb = new StringBuilder((int)difficulty); //for the ui
+            //run through our difficulty counter and create a random stack of input
+            for (int i = 0; i < difficulty; i++){
+                int index = Random.Range(0, 4);
+                _solutionStack.Push(_validKeys[index]);
+                //append input and direction of input
+                sb.Append(_validKeys[index]);
+                if (i != (int)difficulty - 1){
+                    sb.Append('>');
+                }
+            }
+            //reverse the string to get the proper order of input
+            //inputs are added in FILO, so right to left is what they are being removed as, except human beans read left to right so we do this
+            char[] s = sb.ToString().ToCharArray();
+            Array.Reverse(s);
+            inputDirectorField.text = new string(s);
+            inputDirectorField.gameObject.SetActive(true);
         }
 
         /// <summary>
@@ -93,6 +136,7 @@ namespace JulyJam.Interactables{
             needsRepairMarker.SetActive(false);
             irrepairableTimerUI.gameObject.SetActive(false);
             isDestroyedMarker.SetActive(true);
+            inputDirectorField.gameObject.SetActive(false);
             PartDestroyed(_totalShipDamage, this);
         }
 
@@ -102,19 +146,46 @@ namespace JulyJam.Interactables{
         public void RepairObject(){
             needsRepairMarker.SetActive(false);
             irrepairableTimerUI.gameObject.SetActive(false);
+            inputDirectorField.gameObject.SetActive(false);
             PartRepaired(_partHealthDrain);
+            _isInteractable = true;
             _isBroken = false;
         }
 
         /// <summary>
-        /// initiate repairs
+        /// initiate repairs with the primary interact key
         /// </summary>
         /// <param name="player"></param>
         public override void Interact(PlayerMovement player){
             if (_isInteractable && _isBroken){
+                _player = player;
                 _isInteractable = false;
-                player.isInteracting = true;
-                Timing.RunCoroutine(DoRepair(player));
+                _player.isInteracting = true;
+
+                //Timing.RunCoroutine(DoRepair(player));
+            }
+        }
+
+        /// <summary>
+        /// Accept individual key inputs for the repair minigame
+        /// </summary>
+        /// <param name="key"></param>
+        public override void Interact(char key){
+            if (_player != null && _player.isInteracting){
+                //check if the current key is what we pressed
+                if (_solutionStack.Peek() == key){
+                    //visual hooks here
+         
+                    //if so pop
+                    _solutionStack.Pop();
+                    HealShip(_shipRecoverValue); //heal the ship a little since we hit a key successfully
+                }
+                //check for when we are done
+                if (_solutionStack.Count == 0){
+                    RepairObject();
+                    _player.isInteracting = false;
+                    _player = null;
+                }
             }
         }
 
@@ -125,7 +196,6 @@ namespace JulyJam.Interactables{
         /// <returns></returns>
         private IEnumerator<float> DoRepair(PlayerMovement player){
             yield return Timing.WaitForSeconds(timeToInteract);
-            _isInteractable = true;
             player.isInteracting = false;
             RepairObject();
             yield return 0f;
