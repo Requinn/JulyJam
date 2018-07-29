164ec6d2e27a1ff57d02c4605780c7a364f993f1
sesamy <amy.hori97@gmail.com>
2018-07-28 23:05:30 -0700

    audio stuff that doesn't work yet

---
+++ Assets/Player_Footsteps.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Footsteps : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string PlayEvent;
+    bool playerismoving;
+    public float walkingspeed;
+    
+    void Update ()
+    {
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            //Debug.Log("player is moving");
+            playerismoving = true;
+        }
+        else if (Input.GetAxis("Vertical") == 0 || Input.GetAxis("Horizontal") == 0)
+        {
+            //Debug.Log("Player is not moving");
+            playerismoving = false;
+        }
+        }
+
+        void CallFootsteps ()
+    {
+        if (playerismoving == true)
+        {
+            //Debug.Log ("player is moving");
+            FMODUnity.RuntimeManager.PlayOneShot(PlayEvent);
+
+        }
+    }
+
+    void Start ()
+    {
+        InvokeRepeating("CallFootsteps", 0, walkingspeed);
+    }
+    
+    void OnDisable ()
+    {
+        playerismoving = false;
+    }
+    }
+    
+
